a60f96389e9c415d34498df69d425dad63d7c0f3
Urabe <mendez.f.michael@gmail.com>
2019-10-21 11:16:09 -0700

    implementing delete function

--- Project/database testing/app.js
+++ Project/database testing/app.js
@@ -9,15 +9,27 @@ function renderPatientLogIn(doc){
     let li = document.createElement('li');
     let PatientUserName = document.createElement('span');
     let PatientPassword = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('date-id', doc.id);
     PatientUserName.textContent = doc.data().Username;
     PatientPassword.textContent = doc.data().Password;
+    cross.textContent = 'x';
 
     li.appendChild(PatientUserName);
     li.appendChild(PatientPassword);
+    li.appendChild(cross);
 
     PatientLogIn.appendChild(li);
+
+    //deleting data
+    cross.addEventListener('click', (e) => {
+
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('PatientLogIn').doc(id).delete();
+
+    })
 
 }
 //getting data
